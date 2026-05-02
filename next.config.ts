ba57dd012d4a2a753678d8f94f3d78ed8415621f
tanahiro2010 <herentongkegu087@gmail.com>
2026-05-02 14:17:31 +0900

    Revert "全体を通じてリファクタリング"

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Content-Security-Policy",
-            value:
-              process.env.NODE_ENV === "production"
-                ? "default-src 'self'; base-uri 'self'; frame-ancestors 'none'; object-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
-                : "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'",
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,41 @@
-import Button from "@/components/ui/button";
+import { JSX } from "react"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+
+type Props = {
+  children: JSX.Element | string
+  className?: string 
+}
+const Button = ({ children, className = "" }: Props) => {
+  return (
+    <button className={cn(`px-5 py-3 border rounded hover:bg-gray-100 cursor-pointer`, className)}>
+      { children }
+    </button>
+  )
+}
 
 export default function Home() {
   return (
     <main className="h-screen flex flex-col items-center justify-center bg-white">
       <section className="space-y-5">
-        <h1 className="text-5xl font-bold">順番待ち管理システム</h1>
+        <h1 className="text-5xl font-bold">
+          順番待ち管理システム
+        </h1>
 
         <div className="space-x-5 flex justify-center">
-          <Button href={`/login`}>管理パネル</Button>
+          <Link href={`/login`}>
+            <Button>
+              管理パネル
+            </Button>
+          </Link>
 
-          <Button href={`/monitor`}>公衆モニター</Button>
+          <Link href={`/monitor`}>
+            <Button>
+              公衆モニター
+            </Button>
+          </Link>
         </div>
       </section>
     </main>
-  );
+  )
 }
