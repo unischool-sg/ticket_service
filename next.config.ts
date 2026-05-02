3f320dad7090f7165109a37a259b6753b1a5e5d4
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-05-02 12:37:32 +0900

    security: CSPを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              process.env.NODE_ENV === "production"
+                ? "default-src 'self'; base-uri 'self'; frame-ancestors 'none'; object-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
+                : "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
