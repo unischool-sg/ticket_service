98d54a43bc349bec5b25f75548a27a794b102604
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-05-02 17:51:16 +0900

    format

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
-import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
 import LoginClient from "./Client";
 
 export default async function Login() {
