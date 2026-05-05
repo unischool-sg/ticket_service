4707ef394df4feafb5131e1e86f7bd71487935b9
tanahiro2010 <herentongkegu087@gmail.com>
2026-05-05 23:15:31 +0900

    fix: カスタムトースターを使用するようにインポート元のパスを修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Toaster } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const geistSans = Geist({
