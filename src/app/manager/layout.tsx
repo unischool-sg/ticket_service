e4c515b5657f2ddb59914e642df9f8fe57c094e0
tanahiro2010 <herentongkegu087@gmail.com>
2026-05-05 23:13:07 +0900

    fix: ssrなのでauthClientからauthを使用するように修正

--- src/app/manager/layout.tsx
+++ src/app/manager/layout.tsx
@@ -1,13 +1,16 @@
 import { type JSX } from "react";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { authClient } from "@/lib/auth-client";
+import { auth } from "@/lib/auth";
 
 type LayoutProps = {
     children: JSX.Element;
 }
 
 export default async function ManagerLayout({ children }: LayoutProps) {
-    const session = await authClient.getSession();
+    const session = await auth.api.getSession({
+      headers: await headers()
+    });
 
     if (!session.data) {
         redirect("/");
